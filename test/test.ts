969108a565e8eb573005ea1eb700c7655ee79722
Jim Wharton <jamesawharton@gmail.com>
2020-04-05 19:52:24 -0600

    Add tests for remove methods

--- test/test.ts
+++ test/test.ts
@@ -26,4 +26,30 @@ Deno.test({
   }
 });
 
+Deno.test({
+  name: "It removes an edge from the graph",
+  fn(): void {
+    const g = new Graph();
+    g.addNode(1);
+    g.addNode(3);
+    g.addEdge(1,3);
+    g.removeEdge(1,3);
+    assertEquals(g.nodes.length, 2);
+    assertEquals(g.adjList.get(3), []);
+  }
+});
+
+Deno.test({
+  name: "It removes a node from the graph",
+  fn(): void {
+    const g = new Graph();
+    g.addNode(1);
+    g.addNode(3);
+    g.addEdge(1,3);
+    g.removeNode(3);
+    assertEquals(g.nodes.length, 1);
+    assertEquals(g.adjList.get(1), []);
+  }
+});
+
 await Deno.runTests;
